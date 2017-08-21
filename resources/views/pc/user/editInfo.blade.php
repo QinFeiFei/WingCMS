56ec7cdf04da354b4d4063b4e7aa7bd2f3cab1a2
Pengfei Zahng <158133434@qq.com>
2017-08-21 22:47:48 +0800

    home push

--- resources/views/pc/user/editInfo.blade.php
+++ resources/views/pc/user/editInfo.blade.php
@@ -1,6 +1,7 @@
 @extends('pc.layouts.user')
 @section('title', '基本资料')
 @section('head')
+    <script src="https://cdn.bootcss.com/lodash.js/4.17.4/lodash.min.js"></script>
     <link rel="stylesheet" type="text/css" href="{{ asset('/pc/user') }}/useraccount.css">
 @endsection
 
@@ -16,83 +17,47 @@
                 <div class="m-form">
                     <div class="form-item">
                         <span class="form-field ft-bold">真实姓名：</span>
-                        <input class="ipt_txt ipt_defa" name="name" type="text" value="">
+                        <el-input class="ipt_txt" v-model="real_name" placeholder=""></el-input>
                     </div>
                     <div class="form-item">
                         <span class="form-field ft-bold">QQ：</span>
-                        <input class="ipt_txt ipt_defa" type="text" name="qq" value="">
+                        <el-input class="ipt_txt" v-model="qq" placeholder=""></el-input>
                     </div>
                     <div class="form-item form-item-radio">
                         <span class="form-field ft-bold">性别：</span>
-                        <label for="female" class="piece genderItem"><i class="ipt_radio"></i>女士<input style="display: none;" type="radio" name="gender" id="female" value="2"></label>
-                        <label for="male" class="piece genderItem"><i class="ipt_radio ipt_radio_cur"></i>男士<input style="display: none;" type="radio" name="gender" id="male" value="1" checked="checked"></label>
-                        <label class="piece genderItem" for="nomale"><i class="ipt_radio"></i>保密<input style="display: none;" type="radio" name="gender" id="nomale" value="0"></label>
+                        <el-radio-group v-model="real_sex">
+                            <el-radio :label="1">男</el-radio>
+                            <el-radio :label="2">女</el-radio>
+                            <el-radio :label="0">保密</el-radio>
+                        </el-radio-group>
                     </div>
                     <div class="form-item" style="z-index: 10">
                         <span class="form-field ft-bold">生日：</span>
-                        <div class="m-selectA" id="year">
-
-                            <div class="text">年</div>
-                            <div class="holder"><i class="arrow-btm"></i></div>
-                            <div class="option" style="display:none">
-                                <ul>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="m-selectA" id="month">
-                            <input type="hidden" name="month" value="00">
-
-                            <div class="text">月</div>
-                            <div class="holder"><i class="arrow-btm"></i></div>
-                            <div class="option" style="display:none">
-                                <ul></ul>
-                            </div>
-                        </div>
-                        <div class="m-selectA" id="day">
-                            <input type="hidden" name="day" value="00">
-
-                            <div class="text">日</div>
-                            <div class="holder"><i class="arrow-btm"></i></div>
-                            <div class="option" style="display:none">
-                                <ul>
-
-                                </ul>
-                            </div>
-                        </div>
+                        <el-date-picker v-model="real_birthday" type="date" placeholder="选择日期"></el-date-picker>
                     </div>
                     <div class="form-item" style="z-index: 9">
                         <span class="form-field ft-bold">地区：</span>
+                        <el-select v-model="area_selector_selected_prov" placeholder="请选择">
+                            <el-option v-for="(prov, key) in area_selector_provs" :key="key" :label="key" :value="key">
+                                @{{ key }}
+                            </el-option>
+                        </el-select>
 
-                        <div class="m-selectA" id="mcl">
-                            <input type="hidden" name="area1" value="100000">
+                        <el-select v-model="area_selector_selected_city" placeholder="请选择">
+                            <el-option v-for="(city, key) in area_selector_cities" :key="key" :label="key" :value="key">
+                                @{{ key }}
+                            </el-option>
+                        </el-select>
 
-                            <div class="text">省</div>
-                            <div class="holder"><i class="arrow-btm"></i></div>
-                            <div style="display:none" class="option">
-                                <ul><li><a href="http://login.2345.com/member/editData#" value="100000">省</a></li><li><a href="http://login.2345.com/member/editData#" value="110000">北京</a></li><li><a href="http://login.2345.com/member/editData#" value="120000">天津</a></li><li><a href="http://login.2345.com/member/editData#" value="130000">河北省</a></li><li><a href="http://login.2345.com/member/editData#" value="140000">山西省</a></li><li><a href="http://login.2345.com/member/editData#" value="150000">内蒙古自治区</a></li><li><a href="http://login.2345.com/member/editData#" value="210000">辽宁省</a></li><li><a href="http://login.2345.com/member/editData#" value="220000">吉林省</a></li><li><a href="http://login.2345.com/member/editData#" value="230000">黑龙江省</a></li><li><a href="http://login.2345.com/member/editData#" value="310000">上海</a></li><li><a href="http://login.2345.com/member/editData#" value="320000">江苏省</a></li><li><a href="http://login.2345.com/member/editData#" value="330000">浙江省</a></li><li><a href="http://login.2345.com/member/editData#" value="340000">安徽省</a></li><li><a href="http://login.2345.com/member/editData#" value="350000">福建省</a></li><li><a href="http://login.2345.com/member/editData#" value="360000">江西省</a></li><li><a href="http://login.2345.com/member/editData#" value="370000">山东省</a></li><li><a href="http://login.2345.com/member/editData#" value="410000">河南省</a></li><li><a href="http://login.2345.com/member/editData#" value="420000">湖北省</a></li><li><a href="http://login.2345.com/member/editData#" value="430000">湖南省</a></li><li><a href="http://login.2345.com/member/editData#" value="440000">广东省</a></li><li><a href="http://login.2345.com/member/editData#" value="450000">广西壮族自治区</a></li><li><a href="http://login.2345.com/member/editData#" value="460000">海南省</a></li><li><a href="http://login.2345.com/member/editData#" value="500000">重庆</a></li><li><a href="http://login.2345.com/member/editData#" value="510000">四川省</a></li><li><a href="http://login.2345.com/member/editData#" value="520000">贵州省</a></li><li><a href="http://login.2345.com/member/editData#" value="530000">云南省</a></li><li><a href="http://login.2345.com/member/editData#" value="540000">西藏自治区</a></li><li><a href="http://login.2345.com/member/editData#" value="610000">陕西省</a></li><li><a href="http://login.2345.com/member/editData#" value="620000">甘肃省</a></li><li><a href="http://login.2345.com/member/editData#" value="630000">青海省</a></li><li><a href="http://login.2345.com/member/editData#" value="640000">宁夏回族自治区</a></li><li><a href="http://login.2345.com/member/editData#" value="650000">新疆维吾尔自治区</a></li><li><a href="http://login.2345.com/member/editData#" value="710000">台湾省</a></li><li><a href="http://login.2345.com/member/editData#" value="810000">香港特别行政区</a></li><li><a href="http://login.2345.com/member/editData#" value="820000">澳门特别行政区</a></li><li><a href="http://login.2345.com/member/editData#" value="990000">海外</a></li></ul>
-                            </div>
-                        </div>
-                        <div class="m-selectA" id="mcm">
-                            <input type="hidden" name="area2" value="100100">
-
-                            <div class="text">市</div>
-                            <div class="holder"><i class="arrow-btm"></i></div>
-                            <div style="display:none" class="option">
-                                <ul><li><a href="http://login.2345.com/member/editData#" value="100100">市</a></li></ul>
-                            </div>
-                        </div>
-                        <div class="m-selectA" id="mcn">
-                            <input type="hidden" name="area3" value="100101">
-
-                            <div class="text">区县</div>
-                            <div class="holder"><i class="arrow-btm"></i></div>
-                            <div style="display:none" class="option">
-                                <ul><li><a href="http://login.2345.com/member/editData#" value="100101">区县</a></li></ul>
-                            </div>
-                        </div>
+                        <el-select v-model="area_selector_selected_district" placeholder="请选择">
+                            <el-option v-for="(district, key) in area_selector_districts" :key="key" :label="district" :value="district">
+                                @{{ district }}
+                            </el-option>
+                        </el-select>
+                        <p>@{{ area_selector_selected_prov }} @{{ area_selector_selected_city }} @{{ area_selector_selected_district }}</p>
                     </div>
                     <div class="form-item">
-                        <a href="" class="btn-blueA">保存</a>
+                        <el-button type="primary">保存</el-button>
                     </div>
                 </div>
             </div>
@@ -101,11 +66,86 @@
 @endsection
 
 @section('footer')
+<script src="{{ asset('/pc/user/area_selector_data.js') }}"></script>
 <script>
     var vm = new Vue({
         el: '#app',
         data: {
-            test: 'xxx'
+            real_name: '',
+            real_sex: 0,
+            real_birthday: '',
+            real_area: '',
+            qq: '',
+
+            area_selector_data: area_selector_data,
+            area_selector_init_prov: '北京市',
+            area_selector_init_city: '市辖区',
+            area_selector_init_district: '东城区',
+            area_selector_selected_prov: '',
+            area_selector_selected_city: '',
+            area_selector_selected_district: ''
+        },
+        computed: {
+            area_selector_provs: function(){
+                var provs = this.area_selector_data;
+                if (provs){
+                    this.area_selector_selected_prov = _.keys(provs)[0];
+                    return provs;
+                } else {
+                    return {};
+                }
+            },
+            area_selector_cities: function(){
+                var cities = this.area_selector_provs[this.area_selector_selected_prov];
+                if (cities){
+                    this.area_selector_selected_city = _.keys(cities)[0];
+                    return cities;
+                } else {
+                    return {};
+                }
+            },
+            area_selector_districts: function(){
+                var districts = this.area_selector_cities[this.area_selector_selected_city];
+                if (districts) {
+                    this.area_selector_selected_district = districts[0];
+                    return districts;
+                } else {
+                    return [];
+                }
+            }
+        },
+        methods: {
+            areaSelectorSet: function (prov, city, district) {
+                var _this = this;
+
+                var asyncJob = [
+                    function () {
+                        if (_.has(_this.area_selector_provs, prov))
+                            _this.area_selector_selected_prov = prov;
+                    },
+                    function () {
+                        if (_.has(_this.area_selector_cities, city))
+                            _this.area_selector_selected_city = city;
+                    },
+                    function () {
+                        if (_.includes(_this.area_selector_districts, district))
+                            _this.area_selector_selected_district = district;
+                    }
+                ].reduceRight(function (left, right) {
+                    return function () {
+                        right(setTimeout(left, 0))
+                    };
+                });
+
+                asyncJob();
+            }
+        },
+        ready: function(){
+            this.areaSelectorSet(
+                this.area_selector_init_prov,
+                this.area_selector_init_city,
+                this.area_selector_init_district
+            );
         }
     })
 </script>
