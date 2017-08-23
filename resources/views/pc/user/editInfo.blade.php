a480ab85b525581d6c8d43b42921760f7c395ca5
Pengfei Zahng <158133434@qq.com>
2017-08-23 13:06:22 +0800

    home push

--- resources/views/pc/user/editInfo.blade.php
+++ resources/views/pc/user/editInfo.blade.php
@@ -1,8 +1,14 @@
 @extends('pc.layouts.user')
 @section('title', '基本资料')
 @section('head')
-    <script src="https://cdn.bootcss.com/lodash.js/4.17.4/lodash.min.js"></script>
+    <script src="//cdn.bootcss.com/lodash.js/4.17.4/lodash.min.js"></script>
+    <script src="//cdn.bootcss.com/axios/0.16.2/axios.min.js"></script>
     <link rel="stylesheet" type="text/css" href="{{ asset('/pc/user') }}/useraccount.css">
+    <style>
+        .el-breadcrumb:after, .el-breadcrumb:before, .el-button-group:after, .el-button-group:before, .el-form-item:after, .el-form-item:before, .el-form-item__content:after, .el-form-item__content:before {
+            display: none;
+        }
+    </style>
 @endsection
 
 @section('content')
@@ -11,55 +17,49 @@
         @include('pc.user._leftBar')
         <div class="account_right">
             <h2 class="pf">基本资料</h2>
-            {{--<p v-cloak>@{{ test }}</p>--}}
 
             <div class="m-boxB-bd">
-                <div class="m-form">
-                    <div class="form-item">
-                        <span class="form-field ft-bold">真实姓名：</span>
-                        <el-input class="ipt_txt" v-model="real_name" placeholder=""></el-input>
-                    </div>
-                    <div class="form-item">
-                        <span class="form-field ft-bold">QQ：</span>
-                        <el-input class="ipt_txt" v-model="qq" placeholder=""></el-input>
-                    </div>
-                    <div class="form-item form-item-radio">
-                        <span class="form-field ft-bold">性别：</span>
-                        <el-radio-group v-model="real_sex">
+                <el-form :model="forms" :rules="rules" ref="forms" label-position="right" label-width="90px">
+                    <el-form-item label="真实姓名：">
+                        <el-input v-cloak class="ipt_txt" v-model="forms.real_name" placeholder=""></el-input>
+                    </el-form-item>
+                    <el-form-item label="QQ：" prop="qq">
+                        <el-input v-cloak class="ipt_txt" v-model.number="forms.qq" placeholder=""></el-input>
+                    </el-form-item>
+                    <el-form-item label="性别：">
+                        <el-radio-group v-cloak v-model="forms.real_sex">
                             <el-radio :label="1">男</el-radio>
                             <el-radio :label="2">女</el-radio>
                             <el-radio :label="0">保密</el-radio>
                         </el-radio-group>
-                    </div>
-                    <div class="form-item" style="z-index: 10">
-                        <span class="form-field ft-bold">生日：</span>
-                        <el-date-picker v-model="real_birthday" type="date" placeholder="选择日期"></el-date-picker>
-                    </div>
-                    <div class="form-item" style="z-index: 9">
-                        <span class="form-field ft-bold">地区：</span>
-                        <el-select v-model="area_selector_selected_prov" placeholder="请选择">
+                    </el-form-item>
+                    <el-form-item label="生日：">
+                        <el-date-picker v-cloak v-model="forms.real_birthday" type="date" placeholder="选择日期"></el-date-picker>
+                    </el-form-item>
+                    <el-form-item label="地区：">
+                        <el-select v-cloak v-model="area_selector_selected_prov" placeholder="请选择" class="w120">
                             <el-option v-for="(prov, key) in area_selector_provs" :key="key" :label="key" :value="key">
                                 @{{ key }}
                             </el-option>
                         </el-select>
 
-                        <el-select v-model="area_selector_selected_city" placeholder="请选择">
+                        <el-select v-cloak v-model="area_selector_selected_city" placeholder="请选择" class="w120">
                             <el-option v-for="(city, key) in area_selector_cities" :key="key" :label="key" :value="key">
                                 @{{ key }}
                             </el-option>
                         </el-select>
 
-                        <el-select v-model="area_selector_selected_district" placeholder="请选择">
+                        <el-select v-cloak v-model="area_selector_selected_district" placeholder="请选择" class="w120">
                             <el-option v-for="(district, key) in area_selector_districts" :key="key" :label="district" :value="district">
                                 @{{ district }}
                             </el-option>
                         </el-select>
                         <p>@{{ area_selector_selected_prov }} @{{ area_selector_selected_city }} @{{ area_selector_selected_district }}</p>
-                    </div>
+                    </el-form-item>
                     <div class="form-item">
-                        <el-button type="primary">保存</el-button>
+                        <el-button v-cloak @click="save()" v-loading="loading" type="primary">保存</el-button>
                     </div>
-                </div>
+                </el-form>
             </div>
         </div>
     </div>
@@ -71,11 +71,18 @@
     var vm = new Vue({
         el: '#app',
         data: {
-            real_name: '',
-            real_sex: 0,
-            real_birthday: '',
-            real_area: '',
-            qq: '',
+            forms: {
+                real_name: '',
+                real_sex: 0,
+                real_birthday: '',
+                qq: null
+            },
+            rules: {
+                qq: [
+                    { type: 'integer', message: 'QQ号格式错误', trigger: 'blur' }
+                ]
+            },
+            loading: false,
 
             area_selector_data: area_selector_data,
             area_selector_init_prov: '北京市',
@@ -138,6 +145,21 @@
                 });
 
                 asyncJob();
+            },
+            save: function (){
+                this.loading = true
+                var data = {
+                    real_name: this.forms.real_name,
+                    real_sex: this.forms.real_sex,
+                    real_birthday: this.forms.real_birthday,
+                    qq: this.forms.qq,
+                    real_area: this.area_selector_selected_prov + ' ' + this.area_selector_selected_city + ' ' + this.area_selector_selected_district
+                }
+                axios({
+                    method: 'post',
+                    url: '{{ 'pc::userEditInfo' }}',
+                    data: data
+                });
             }
         },
         ready: function(){
